588ce5837aa823e7a7b37ad8cfa399259d01617d
eddievandeer <1255173159@qq.com>
2023-01-01 20:20:26 +0800

    :bulb: Update comment in 7 and 11

--- type-challenges/11-easy-tuple-to-object/template.ts
+++ type-challenges/11-easy-tuple-to-object/template.ts
@@ -1,6 +1,6 @@
 // 关键词：
-// - typeof
-// - [number]
+// - typeof: 将 js 的值转为 ts 的类型
+// - [number]: 用于获取数组的每一个项的值，keyof 只能获取索引
 type TupleToObject<T extends readonly (number | string | symbol)[]> = {
   [P in T[number]]: P;
 };
--- type-challenges/7-easy-readonly/template.ts
+++ type-challenges/7-easy-readonly/template.ts
@@ -1,3 +1,4 @@
+// 关键字：Mapped Types、lookup
 type MyReadonly<T> = {
   readonly [K in keyof T]: T[K];
 };
